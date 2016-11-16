adceca8e1a0ec6995ee78c49cdc340101d080054
Acshi Haggenmiller <acshikh@gmail.com>
2016-11-16 02:28:31 -0500

    laser pointer added, trigger script fixed, please do not delete (instead delete laserpointer script in Assets/SteamVR/Extras)

--- Assets/triggerScript.cs
+++ Assets/triggerScript.cs
@@ -12,46 +12,50 @@ public class triggerScript : MonoBehaviour {
     private Vector3 positionDifference;
     private float increment = .1f;
 
-    private SteamVR_Controller.Device device;
+	private SteamVR_Controller.Device device = null;
 
     // Use this for initialization
     void Awake() {
         trackedObject = GetComponent<SteamVR_TrackedObject>();
-        device = SteamVR_Controller.Input((int)trackedObject.index);
-		Debug.Log ("awake and set device");
     }
 
     // Update is called once per frame
     void FixedUpdate() {
-        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
-            Debug.Log("got press");
-            procedural.transform.position = transform.position + positionDifference;
-        } else {
-            positionDifference = procedural.transform.position - transform.position;
-        }
+		if (device != null) {
+			if (trackedObject.index != SteamVR_TrackedObject.EIndex.None) {
+				device = SteamVR_Controller.Input ((int)trackedObject.index);
+			} else {
+				if (device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+					Debug.Log ("got press");
+					procedural.transform.position = transform.position + positionDifference;
+				} else {
+					positionDifference = procedural.transform.position - transform.position;
+				}
 
 
-        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
-            Vector2 coords = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
-            //if zoom
-            if (coords.x > -.2f && coords.x < .2f) {
-                if (coords.y < 0) {
-                    Debug.Log("bottom");
-                    buildMesh.zoomIn(-increment);
-                } else {
-                    Debug.Log("top");
-                    buildMesh.zoomIn(increment);
-                }
-            } else {
-                if (coords.x < 0) {
-                    Debug.Log("left");
-                    procedural.transform.Rotate(0, 10, 0);
-                } else {
-                    Debug.Log("right");
-                    procedural.transform.Rotate(0, -10, 0);
-                }
-            }
-        }
+				if (device.GetPressDown (SteamVR_Controller.ButtonMask.Touchpad)) {
+					Vector2 coords = device.GetAxis (EVRButtonId.k_EButton_SteamVR_Touchpad);
+					//if zoom
+					if (coords.x > -.2f && coords.x < .2f) {
+						if (coords.y < 0) {
+							Debug.Log ("bottom");
+							buildMesh.zoomIn (-increment);
+						} else {
+							Debug.Log ("top");
+							buildMesh.zoomIn (increment);
+						}
+					} else {
+						if (coords.x < 0) {
+							Debug.Log ("left");
+							procedural.transform.Rotate (0, 10, 0);
+						} else {
+							Debug.Log ("right");
+							procedural.transform.Rotate (0, -10, 0);
+						}
+					}
+				}
+			}
+		}
     }
 
     void OnCollisionEnter(Collision col) {
