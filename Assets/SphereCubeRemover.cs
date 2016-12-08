aef4e14bb72eec2010740be6f7441ca8bd7de68d
lihenryhfl <li.henry.hfl@gmail.com>
2016-12-07 21:36:37 -0500

    added SphereCubeRemove (not tested either)

---
+++ Assets/SphereCubeRemover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCubeRemover : MonoBehaviour {
+	private GameObject sphere;
+	private MeshCollider sphereMC;
+	public bool isVisible = true;
+	public bool isTest = true;
+
+	// Use this for initialization
+	void Start () {
+		sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		sphereMC = sphere.AddComponent<MeshCollider>() as MeshCollider;
+		sphereMC.convex = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isVisible) {
+			sphere.transform.position = gameObject.transform.position;
+		}
+	}
+
+	void setVisibility(bool isVisible) {
+		this.isVisible = isVisible;
+		sphere.SetActive(isVisible);
+		sphereMC.convex = isVisible;
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (!isVisible) {
+			Debug.Log("still in onCollisionEnter");
+			return;
+		}
+		Debug.Log("entered OnCollisionEnter");
+		// We just take the first collision point and ignore others
+		ContactPoint P = col.contacts[0];
+		RaycastHit hit;
+		Ray ray = new Ray(P.point + P.normal * 0.05f, -P.normal);
+		if (P.otherCollider.Raycast(ray, out hit, 0.1f))
+		{
+			int triangle = hit.triangleIndex;
+			Debug.Log("Got triangle: " + triangle);
+			// do something...
+		}
+		else
+			Debug.LogError("Have a collision but can't raycast the point");
+	}
+}
